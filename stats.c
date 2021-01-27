75f91204dd196fcd360d03a963c800aa6392edb0
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-01-27 11:07:26 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -43,7 +43,7 @@ computedStats compute_statistics(const float* numberset, int setlength) {
 	{
 		s.average = isnanf(s.average/s.min);
 		s.min = isnanf(s.average/s.min);
-		s.max = isnanf(s.average/s.min);
+		s.max = isnanf(s.average/s.max);
 		return s;
 	}
 		
